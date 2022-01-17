5e6569793a7f9b6ea49658f20146cd1923f39e42
felixhc <felixhumbertocarabajal5@gmail.com>
2022-01-17 23:48:13 +0000

    Feat: Adding Backend Structure

---
+++ backend/app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from .db_conection import Base
+
+
+class Car(Base):
+    __tablename__ = "car"
+
+    id: str = Column(String, primary_key=True, index=True)
+    car_name: str = Column(String, unique=True, index=True)
+    car_plate: str = Column(String, unique=True, index=True)
